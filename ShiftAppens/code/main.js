19d175a8fdbde8bf197dbe3193560a4ef47af83d
Miguel Marques <38664523+miguelpmmarques@users.noreply.github.com>
2019-04-06 13:43:08 +0100

    v0

--- ShiftAppens/code/main.js
+++ ShiftAppens/code/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const totPages = 3;
+
 
 (function()
 {
@@ -10,58 +10,5 @@ const totPages = 3;
 
 function main()
 {
-	var btn = document.getElementsByTagName("button")[0];
-	btn.addEventListener("click", btnNextPageHandler);  //escutar clicks no botão de navegação
-
-	window.addEventListener("message", messageHandler);
-
-	var startPage = 1;
-	showPage(startPage);
-}
-
-
-//---- Navegação e gestão de janelas
-function showPage(pageNum)
-{
-	//carregar página na frame e enviar mensagem para a página logo que esteja carregada (frameLoadHandler)
-	var frm = document.getElementsByTagName("iframe")[0];
-	frm.src = "./gyroTests/gyroAcel.html";
-
-	setTimeout(function(){frm.contentWindow.postMessage("", "*");}, 3000);
-
-	if(pageNum == totPages) //se última, esconder botão de navegação
-	{
-		var btn = document.getElementsByTagName("button")[0];
-		btn.style.visibility = "hidden";
-		btn.removeEventListener("click", btnNextPageHandler);  //remover clicks no botão de navegação
-	}
-}
-
-function hidePage(pageNum)  //não é necessário (excepto se páginas diferentes tivessem zonas de navegação diferentes)
-{
-	var frm = document.getElementsByTagName("iframe")[0];
-	frm.src = "";
-}
-
-function btnNextPageHandler(ev)
-{
-	var frm = document.getElementsByTagName("iframe")[0];
-	var frmName = frm.src;
-	var pageNum = Number(frmName.charAt(frmName.length - 6));  //e.g., page2.html --> get the number 2
-
-	if (pageNum != totPages){
-		hidePage(pageNum);
-		showPage(pageNum + 1);
-	}
-	else
-		//close last frame
-		close(frm);
-
-	return pageNum + 1;
-}
-
-function messageHandler(ev)
-{
-	//advance in the frames
-	var current = btnNextPageHandler(ev);
+	console.log("Load");
 }
