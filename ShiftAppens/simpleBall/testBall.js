6ae6ac358926d2751a87b21305d099eb28122d2f
Miguel Marques <38664523+miguelpmmarques@users.noreply.github.com>
2019-04-06 15:50:17 +0100

    d

--- ShiftAppens/simpleBall/testBall.js
+++ ShiftAppens/simpleBall/testBall.js
@@ -4,20 +4,23 @@ var output = document.querySelector('.output');
 
 var maxX = garden.clientWidth  - ball.clientWidth;
 var maxY = garden.clientHeight - ball.clientHeight;
-var calibate = window.getElementById("calibrate")
+var calibate = document.getElementById("calibrate")
 var x;  // In degree in the range [-180,180]
 var y;
 
-function handleOrientation(event) {
-    function reset(ev) {
+function reset(ev) {
+    maxX = garden.clientWidth  - ball.clientWidth;
+    maxY = garden.clientHeight - ball.clientHeight;
+    x = 0;  // In degree in the range [-180,180]
+    y = 0;
 
-        output.innerHTML += "gamma: " + y + "\n";
-    }
+}
+function handleOrientation(event) {
     calibate.addEventListener("click",reset)
     x = event.beta;  // In degree in the range [-180,180]
     y = event.gamma; // In degree in the range [-90,90]
 
-  output.innerHTML  = "beta : " + x + "\n";
+  output.innerHTML  += "beta : " + x + "\n";
   output.innerHTML += "gamma: " + y + "\n";
 
   // Because we don't want to have the device upside down
