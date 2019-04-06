c71034338fee4484b69e2731580e421e2c64bd71
MiguelSeabra <44583767+MiguelSeabra@users.noreply.github.com>
2019-04-06 23:29:26 +0100

    Update testBall.js

--- ShiftAppens/testesMiguel/testBall.js
+++ ShiftAppens/testesMiguel/testBall.js
@@ -25,13 +25,13 @@ function orientationHandler(event) {
   output.innerHTML  = "beta : " + x + "\n";
   output.innerHTML  +="gamma: " + y + "\n";
   output.innerHTML +="alpha: " + z + "\nUltimo\n";
-
+  y += 90;
   // Because we don't want to have the device upside down
   // We constrain the x value to the range [-90,90]
   if (y >  90) { y =  90};
   if (y < -90) { y = -90};
 
-  y += 90;
+  
 
   // 10 is half the size of the ball
   // It center the positioning point to the center of the ball
