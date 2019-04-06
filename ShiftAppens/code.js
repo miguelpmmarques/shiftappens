b43b9d7b5def8b49ad9ebbfdf00a016f7791cb8f
Miguel Marques <38664523+miguelpmmarques@users.noreply.github.com>
2019-04-06 04:45:41 +0100

    DataBaseTest

--- ShiftAppens/code.js
+++ ShiftAppens/code.js
@@ -1,91 +1,38 @@
-/* The dragging code for '.draggable' from the demo above
- * applies to this demo as well so it doesn't have to be repeated. */
+"use strict";
 
-// enable draggables to be dropped into this
-interact('.dropzone').dropzone({
-  // only accept elements matching this CSS selector
-  accept: '#yes-drop',
-  // Require a 75% element overlap for a drop to be possible
-  overlap: 0.75,
+const path = "../assets/";
 
-  // listen for drop related events:
 
-  ondropactivate: function (event) {
-    // add active dropzone feedback
-    event.target.classList.add('.dropzone')
-  },
-  ondragenter: function (event) {
-      console.log("bateu1");
-    var seconds = 5;
+(function()
+{
+	window.addEventListener("load", main);
+}());
 
-    var draggableElement = event.relatedTarget;
-    var dropzoneElement = event.target;
 
-    // feedback the possibility of a drop
-    draggableElement.classList.add('dropztrigger')
-    draggableElement.textContent = 'WILL START'
+function main()
+{
+    var setUpObject = new SetUp()
+	var one = document.getElementById('o');
+    var two = document.getElementById('tw');
+    var three = document.getElementById('th');
+
+    if (localStorage.getItem("items") === null) {
+		localStorage.setItem('items',JSON.stringify(setUpObject));
+	}
+    var localDataBase = JSON.parse(localStorage.getItem('items'));
+    one.innerHTML = "teste -> "+localDataBase.nameTeste;
 
 
 
-    console.log("Entrou aqui");
-    var t = window.setInterval(add, 1000);
 
-    function add() {
-    seconds--;
-        if (seconds < 0) {
-            //window.alert("Tenho pelos no rabo");
-            window.clearInterval(t);
+    one.addEventListener("click",buttonControler)
 
-        }
-        console.log(t);
-        console.log("Bateu -> "+seconds);
-        draggableElement.textContent = "0"+seconds > 9 ? seconds : "0" + seconds;
+    function buttonControler() {
+        var n = Math.random();
+        console.log("Bateu");
+        one.innerHTML = "teste -> "+n;
+        localDataBase.nameTeste = n;
+        localStorage.setItem('items',JSON.stringify(localDataBase));
     }
 
-
-  },
-  ondragleave: function (event) {
-      window.clearInterval(t);
-    // remove the drop feedback style
-    event.target.classList.remove('drop-target')
-    event.relatedTarget.classList.remove('can-drop')
-    event.relatedTarget.textContent = 'START'
-  },
-
-  ondropdeactivate: function (event) {
-    // remove active dropzone feedback
-    event.target.classList.remove('drop-active')
-    event.target.classList.remove('drop-target')
-  }
-});
-interact('.drag-drop')
-  .draggable({
-    inertia: true,
-    modifiers: [
-      interact.modifiers.restrict({
-        restriction: "parent",
-        endOnly: true,
-        elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-      })
-    ],
-    autoScroll: true,
-    // dragMoveListener from the dragging demo above
-    onmove: dragMoveListener,
-  });
-
-function dragMoveListener (event) {
-
-    var target = event.target,
-        // keep the dragged position in the data-x/data-y attributes
-        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-    // translate the element
-    target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
-
-    // update the posiion attributes
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-  }
+}
---
+++ ShiftAppens/setUp.js
@@ -0,0 +1,9 @@
+"use strict";
+
+class SetUp
+{
+	constructor()
+	{
+		this.nameTeste = 0;
+	}
+}
