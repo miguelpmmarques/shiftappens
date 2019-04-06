1f4e736e75fc8fbfb0e4471d791ffdb29efbb4a3
Miguel Marques <38664523+miguelpmmarques@users.noreply.github.com>
2019-04-06 15:26:26 +0100

    oioi

--- ShiftAppens/simpleBall/testBall.js
+++ ShiftAppens/simpleBall/testBall.js
@@ -5,16 +5,16 @@ var output = document.querySelector('.output');
 var maxX = garden.clientWidth  - ball.clientWidth;
 var maxY = garden.clientHeight - ball.clientHeight;
 var calibate = document.getElementById("calibrate")
+calibate.addEventListener("click",reset)
+
+function reset(ev) {
+    var maxX = garden.clientWidth  - ball.clientWidth;
+    var maxY = garden.clientHeight - ball.clientHeight;
+
+}
 function handleOrientation(event) {
   var x = event.beta;  // In degree in the range [-180,180]
   var y = event.gamma; // In degree in the range [-90,90]
-  calibate.addEventListener("click",reset)
-
-  function reset(ev) {
-      var maxX = garden.clientWidth  - ball.clientWidth;
-      var maxY = garden.clientHeight - ball.clientHeight;
-
-  }
 
   output.innerHTML  = "beta : " + x + "\n";
   output.innerHTML += "gamma: " + y + "\n";
