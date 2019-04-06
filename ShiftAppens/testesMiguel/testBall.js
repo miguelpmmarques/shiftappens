612ee2541cf5d1eb4abc806754df252e37d30222
MiguelSeabra <44583767+MiguelSeabra@users.noreply.github.com>
2019-04-06 17:24:30 +0100

    Update testBall.js

--- ShiftAppens/testesMiguel/testBall.js
+++ ShiftAppens/testesMiguel/testBall.js
@@ -3,8 +3,8 @@ var garden = document.querySelector('.garden');
 var output = document.querySelector('.output');
 var paddle1 = document.querySelector('.rectangle');
 
-var maxX = garden.clientWidth  - ball.clientWidth;
-var maxY = garden.clientHeight - ball.clientHeight;
+var maxX = garden.clientWidth  - rectangle.clientWidth;
+var maxY = garden.clientHeight - rectangle.clientHeight;
 
 var x;  // In degree in the range [-180,180]
 var y;
