924509b464918bb1bcc7a38fc651b596f999c10f
Miguel Marques <38664523+miguelpmmarques@users.noreply.github.com>
2019-04-06 17:29:53 +0100

    Come on

--- ShiftAppens/simpleBall/testBall.js
+++ ShiftAppens/simpleBall/testBall.js
@@ -4,7 +4,7 @@ var output = document.querySelector('.output');
 var x;
 var y;
 
-var maxX = garden.clientWidth  - ball.clientWidth;
+var //maxX = garden.clientWidth  - ball.clientWidth;
 var maxY = garden.clientHeight - ball.clientHeight;
 var calibrate = document.getElementById("calibrate")
 
@@ -12,7 +12,7 @@ var debug1 = document.getElementById("debug1")
 
 
 function handleOrientation(event) {
-  x = event.beta;  // In degree in the range [-180,180]
+  //x = event.beta;  // In degree in the range [-180,180]
   y = event.gamma; // In degree in the range [-90,90]
   calibrate.addEventListener("click",reset)
 
@@ -20,29 +20,29 @@ function handleOrientation(event) {
       debug1.innerHTML = "-> "+Math.random()
       ball   = document.querySelector('.ball');
       garden = document.querySelector('.garden');
-      maxX = garden.clientWidth  - ball.clientWidth;
+      //maxX = garden.clientWidth  - ball.clientWidth;
       maxY = garden.clientHeight - ball.clientHeight;
-      x = 0;  // In degree in the range [-180,180]
+      //x = 0;  // In degree in the range [-180,180]
       y = 0;
   }
 
-  output.innerHTML  = "beta : " + x + "\n";
+  //output.innerHTML  = "beta : " + x + "\n";
   output.innerHTML += "gamma: " + y + "\n";
 
   // Because we don't want to have the device upside down
   // We constrain the x value to the range [-90,90]
-  if (x >  90) { x =  90};
-  if (x < -90) { x = -90};
+  //if (x >  90) { x =  90};
+  //if (x < -90) { x = -90};
 
   // To make computation easier we shift the range of
   // x and y to [0,180]
-  x += 90;
+  //x += 90;
   y += 90;
 
   // 10 is half the size of the ball
   // It center the positioning point to the center of the ball
-  //ball.style.top  = (maxX*x/180 - 10) + "px";
-  ball.style.top = (-maxY*y/180 - 10) + "px";
+ // ball.style.top  = (maxX*x/180 - 10) + "px";
+  ball.style.left = (-maxY*y/180 - 10) + "px";
 }
 
 window.addEventListener('deviceorientation', handleOrientation);
