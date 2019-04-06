b6388585c31d5850f57493d0c0e19f69b21726cc
MiguelSeabra <44583767+MiguelSeabra@users.noreply.github.com>
2019-04-06 17:09:59 +0100

    Update testBall.js

--- ShiftAppens/testesMiguel/testBall.js
+++ ShiftAppens/testesMiguel/testBall.js
@@ -30,8 +30,8 @@ function handleOrientation(event) {
 
   // 10 is half the size of the ball
   // It center the positioning point to the center of the ball
-  square.style.top  = (maxX*x/180 - 10) + "px";
-  square.style.left = (maxY*y/180 - 10) + "px";
+  paddle1.style.top  = (maxX*x/180 - 10) + "px";
+  paddle1.style.left = (maxY*y/180 - 10) + "px";
 }
 
 window.addEventListener('deviceorientation', handleOrientation);
