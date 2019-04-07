3cad070a0531d9687e75aa721f17b8a2492a20d3
MiguelSeabra <44583767+MiguelSeabra@users.noreply.github.com>
2019-04-07 01:36:46 +0100

    Update testBall.js

--- ShiftAppens/testesMiguel/testBall.js
+++ ShiftAppens/testesMiguel/testBall.js
@@ -66,7 +66,7 @@ function motionHandler(event){
 }
 function main(){
   var sprite = document.getElementById('sprite');
-  sprite.source = imgFolder + "6.png";
+  sprite.src = imgFolder + "6.png";
   var maxX = garden.clientWidth  - ball.clientWidth;
   var maxY = garden.clientHeight - ball.clientHeight;
   var xcount = 0;
@@ -112,18 +112,18 @@ function main(){
     if(gamma <gamma_max1 && gamma > gamma_min1 && beta < beta_max1 && beta > beta_min1){
 
       ball2.style.background = "red";
-      sprite.source = imgFolder + "13.png";
+      sprite.src = imgFolder + "13.png";
     }
     else{
       ball2.style.background = "blue";
-      sprite.source = imgFolder + "6.png"
+      sprite.src = imgFolder + "6.png"
     }
     if(gamma  > gamma_min2 && gamma <gamma_max2 && beta < beta_max2 && beta > beta_min2){
       ball3.style.background ="red";
-      sprite.source = imgFolder + "20.png";
+      sprite.src = imgFolder + "20.png";
     }else{
       ball3.style.background = "blue";
-      sprite.source = imgFolder + "6.png"
+      sprite.src = imgFolder + "6.png"
     }
 
   
