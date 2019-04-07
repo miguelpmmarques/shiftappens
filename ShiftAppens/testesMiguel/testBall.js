68d1c4aa51e2bdc5c38f7bbc4ec384d4ce84b58d
MiguelSeabra <44583767+MiguelSeabra@users.noreply.github.com>
2019-04-07 04:56:06 +0100

    8y8

--- ShiftAppens/testesMiguel/testBall.js
+++ ShiftAppens/testesMiguel/testBall.js
@@ -108,12 +108,12 @@ function main(){
     if(gamma <gamma_max1 && gamma > gamma_min1 && beta < beta_max1 && beta > beta_min1){
       var h = (gamma_max1 - gamma_min1)/4;
       var aux = Math.round(gamma - gamma_min1);
-      var n =Math.round( aux % h); 
+      var n =Math.round( aux / h); 
       output.innerHTML ="gamma :" + gamma + "\n" + "n: " + n + "\n" + "h: " + h+ "\n" + "aux: " + aux + "\n";
 
 
       ball2.style.background = "green";
-      sprite.src = imgFolder + "13.png";
+      sprite.src = imgFolder + 13 + n + ".png";
       
     }
     else if(gamma  > gamma_min2 && gamma <gamma_max2 && beta < beta_max2 && beta > beta_min2){
