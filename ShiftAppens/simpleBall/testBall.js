2e1a7ae1821c923db8298b9f457521f00a3ed85c
Miguel Marques <38664523+miguelpmmarques@users.noreply.github.com>
2019-04-07 02:11:15 +0100

    more test

--- ShiftAppens/simpleBall/testBall.js
+++ ShiftAppens/simpleBall/testBall.js
@@ -56,7 +56,7 @@ function main() {
         bally += dy;
         if (colision_detect(ballx,bally,10,10,y+20, 20, 20, 60)) {
             window.alert("Cona");
-
+            dy = -dy;
         }
 
     }
